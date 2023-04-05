058748fda94b20a44726f38a761715effe5b059c
Dan Damian <arcassis@gmail.com>
2023-04-05 19:02:45 +0300

    Almost there

--- src/App.js
+++ src/App.js
@@ -2,32 +2,48 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { API, graphqlOperation } from '@aws-amplify/api'
+import { Auth } from "aws-amplify";
 import config from './aws-exports'
 import { subscribe } from './graphql/subscriptions'
 
-API.configure(config)
+Auth.configure({
+  region: 'eu-central-1',
+  userPoolId: 'eu-central-1_29CFknuNU',
+  userPoolWebClientId: '2i1l8vc0nl3eba5h8m6t2buftr',
+  mandatorySignIn: true,
+  authenticationFlowType: 'USER_SRP_AUTH'
+})
 
+console.log(await Auth.signIn("frontend", "P@ssw0rd"));
+
+API.configure({
+  ...config,
+  Auth
+})
 const handleNotification = (notification) => {
   console.log('New notification:', notification);
 };
 
-
 function App() {
-  useEffect(() => {
+
+  useEffect( () => {
+
     const subscription = API.graphql({
         query: subscribe,
         variables: {
-          name: "channelTwo"
+          name: "channelOne"
         }}
     ).subscribe({
       error: (err) => {
-        console.log("Subscription error", err)
+        console.log("Subscription error", JSON.stringify(err))
         // setStatus("Disconnected :(")
       },
       next: (notificationData) => {
         handleNotification(notificationData.value.data.subscribe);
       },
     });
+
+    console.log("Successfully subscribed.");
 
     return () => subscription.unsubscribe();
   }, []);
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_appsync_graphqlEndpoint": process.env.REACT_APP_aws_appsync_graphqlEndpoint,
     "aws_appsync_region": process.env.REACT_APP_aws_appsync_region,
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": process.env.REACT_APP_aws_appsync_apiKey,
+    "aws_appsync_authenticationType": 'AMAZON_COGNITO_USER_POOLS',
+    "aws_appsync_apiKey": null,
 };
 
 export default awsmobile;
